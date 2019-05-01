de5f406d858e12d1b421d3cf27a5c39d252bb9df
StevenVandy <smvandeventer@gmail.com>
2019-05-01 15:34:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-udemyproject
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-udemyproject)
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('A test Recipe', 'test desc', 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
